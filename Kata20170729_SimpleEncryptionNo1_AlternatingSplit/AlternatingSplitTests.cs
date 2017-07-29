7b1c5bf2c83be77ce45d5822b6e9f7600a802aad
cash <cash@cashwu.com>
2017-07-29 13:08:31 +0800

    init

--- Kata20170729_SimpleEncryptionNo1_AlternatingSplit/AlternatingSplitTests.cs
+++ Kata20170729_SimpleEncryptionNo1_AlternatingSplit/AlternatingSplitTests.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿    using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170729_SimpleEncryptionNo1_AlternatingSplit
 {
     [TestClass]
-    public class UnitTest1
+    public class AlternatingSplitTests
     {
         [TestMethod]
         public void TestMethod1()
