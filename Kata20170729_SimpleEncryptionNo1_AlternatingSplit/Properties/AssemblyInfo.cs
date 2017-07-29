12348a2504fd895a72fc9ca78c1ff57ae6f2b988
cash <cash@cashwu.com>
2017-07-29 13:07:49 +0800

    Add project files.

---
+++ Kata20170729_SimpleEncryptionNo1_AlternatingSplit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kata20170729_SimpleEncryptionNo1_AlternatingSplit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kata20170729_SimpleEncryptionNo1_AlternatingSplit")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8346908c-8ffb-4101-9aa7-31f3872e50f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Kata20170729_SimpleEncryptionNo1_AlternatingSplit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170729_SimpleEncryptionNo1_AlternatingSplit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
